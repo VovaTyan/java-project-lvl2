f060c30fddcd5160d50b62e10610cc4af965de3d
Vova Tyan <Tyan.volodia@yandex.ru>
2022-08-05 21:15:42 +0300

    step 6.6

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -11,8 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DifferTest {
     @Test
     public void testGenerate() throws IOException {
-        Path path1 = Path.of("/home/vova/java-project-lvl2/file1.json");
-        Path path2 = Path.of("/home/vova/java-project-lvl2/file2.json");
+        Path path1 = Path.of("file1.json");
+        Path path2 = Path.of("file2.json");
         var actual1 = Differ.generate(path1, path2);
         var expected1 = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
                 + "  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
