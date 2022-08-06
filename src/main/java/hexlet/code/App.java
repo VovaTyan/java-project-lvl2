96a95f83759e57bd07cd2a94a2b116aa9a963694
Vova Tyan <Tyan.volodia@yandex.ru>
2022-08-06 16:45:57 +0300

    step 8

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,8 +2,6 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -14,15 +12,30 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 class Gendiff implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "path to first file")
-        private Path filepath1;
-    @Parameters(index = "1", description = "path to second file")
-        private Path filepath2;
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
         private String format = "stylish";
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+        private Path filepath1;
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+        private Path filepath2;
+
     @Override
     public Integer call() throws IOException {
-        var diff = Differ.generate(filepath1, filepath2);
+        String diff = "";
+        switch (format) {
+            case "stylish":
+                diff = Differ.generate(filepath1, filepath2);
+                break;
+            case "plain":
+                diff = "";
+                break;
+            case "json":
+                diff = " ";
+                break;
+            default:
+                diff = "Неверный формат";
+        }
+
         System.out.println(diff);
         return 0;
     }
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,9 +13,11 @@ import java.util.TreeSet;
 public class Differ {
     public static String generate(Path filepath1, Path filepath2) throws IOException {
         String fileString1 = Files.readString(filepath1);
-        Map<String, Object> oneMap = new ObjectMapper().readValue(fileString1, new TypeReference<>() { });
+        Map<String, Object> oneMap = new ObjectMapper(new YAMLFactory())
+                .readValue(fileString1, new TypeReference<>() { });
         String fileString2 = Files.readString(filepath2);
-        Map<String, Object> twoMap = new ObjectMapper().readValue(fileString2, new TypeReference<>() { });
+        Map<String, Object> twoMap = new ObjectMapper(new YAMLFactory())
+                .readValue(fileString2, new TypeReference<>() { });
 
         StringBuilder result = new StringBuilder();
 
@@ -25,16 +28,19 @@ public class Differ {
 
         result.append("{\n");
         for (String key: keys) {
+            String oneString = oneMap.get(key) == null ? "null" : oneMap.get(key).toString();
+            String twoString = twoMap.get(key) == null ? "null" : twoMap.get(key).toString();
+
             if (!oneMap.containsKey(key)) {
-                result.append("  + ").append(key).append(": ").append(twoMap.get(key)).append("\n");
+                result.append("  + ").append(key).append(": ").append(twoString).append("\n");
             } else if (!twoMap.containsKey(key)) {
-                result.append("  - ").append(key).append(": ").append(oneMap.get(key)).append("\n");
+                result.append("  - ").append(key).append(": ").append(oneString).append("\n");
             } else {
-                if (!twoMap.get(key).equals(oneMap.get(key))) {
-                    result.append("  - ").append(key).append(": ").append(oneMap.get(key)).append("\n");
-                    result.append("  + ").append(key).append(": ").append(twoMap.get(key)).append("\n");
+                if (!oneString.equals(twoString)) {
+                    result.append("  - ").append(key).append(": ").append(oneString).append("\n");
+                    result.append("  + ").append(key).append(": ").append(twoString).append("\n");
                 } else {
-                    result.append("    ").append(key).append(": ").append(oneMap.get(key)).append("\n");
+                    result.append("    ").append(key).append(": ").append(oneString).append("\n");
                 }
             }
         }
