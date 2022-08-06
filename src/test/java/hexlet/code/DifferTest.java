fe84185427789d9eff902269a8e93651a2bc3e80
Vova Tyan <Tyan.volodia@yandex.ru>
2022-08-06 23:34:22 +0300

    step 9.3

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -13,15 +13,8 @@ class DifferTest {
         Path path1 = Path.of("file1.json");
         Path path2 = Path.of("file2.json");
         var actual1 = Differ.generate(path1, path2, "stylish");
-        var expected1 = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
+        var expected1 = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
         assertThat(actual1).isEqualTo(expected1);
     }
 
@@ -30,15 +23,8 @@ class DifferTest {
         Path path1 = Path.of("file1.yml");
         Path path2 = Path.of("file2.yml");
         var actual1 = Differ.generate(path1, path2, "stylish");
-        var expected1 = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
+        var expected1 = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
         assertThat(actual1).isEqualTo(expected1);
     }
 
@@ -47,13 +33,11 @@ class DifferTest {
         Path path1 = Path.of("file1.json");
         Path path2 = Path.of("file2.json");
         var actual1 = Differ.generate(path1, path2, "plain");
-        var expected1 = """
-                Property 'follow' was removed
-                Property 'host' was unchanged with value: hexlet.io
-                Property 'proxy' was removed
-                Property 'timeout' was updated. From 50 to 20
-                Property 'verbose' was added with value: true
-                """;
+        var expected1 = "Property 'follow' was removed\n"
+                + "Property 'host' was unchanged with value: hexlet.io\n"
+                + "Property 'proxy' was removed\n"
+                + "Property 'timeout' was updated. From 50 to 20\n"
+                + "Property 'verbose' was added with value: true\n";
         assertThat(actual1).isEqualTo(expected1);
     }
 }
