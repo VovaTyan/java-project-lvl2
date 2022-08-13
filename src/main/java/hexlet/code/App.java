e5000629e285c2cef1a23216a1eccde54bd1b45e
Vova Tyan <Tyan.volodia@yandex.ru>
2022-08-13 10:33:46 +0300

    change folder to app

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -13,9 +13,9 @@ class Gendiff implements Callable<Integer> {
 
     @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
         private String format = "stylish";
-    @CommandLine.Parameters(index = "0", description = "path to first file")
+    @CommandLine.Parameters(index = "0", description = "path to first file", defaultValue = "file1.json")
         private Path filepath1;
-    @CommandLine.Parameters(index = "1", description = "path to second file")
+    @CommandLine.Parameters(index = "1", description = "path to second file", defaultValue = "file2.json")
         private Path filepath2;
 
     @Override
